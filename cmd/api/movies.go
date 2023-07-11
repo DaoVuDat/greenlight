70877adb036ae4b3946b1d33d959b8953fa09c45
Đào Vũ Đạt <dat.daovu@gmail.com>
2023-07-11 15:11:54 +0700

    add error response

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,8 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		// Use the new notFoundResponse() helper.
+		app.notFoundResponse(w, r)
 		return
 	}
 
@@ -35,7 +36,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	// of passing the plain movie struct.
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		// Use the new serverErrorResponse() helper.
+		app.serverErrorResponse(w, r, err)
 	}
 }
